103106106d8531acc939762611a182a7e8d2caee
Stefan Liute <stefanliute@gmail.com>
2017-01-05 23:01:30 +0000

    Challenges 14-17

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,15 +2,35 @@ require_relative 'bike'
 
 class DockingStation
 
-	attr_reader :bike
+	DEFAULT_CAPACITY = 20
+
+	attr_reader :capacity
+
+	def initialize(capacity=DEFAULT_CAPACITY)
+		@bikes = []
+		@capacity = capacity
+	end
+
+	private
+
+	def full?
+		@bikes.length >= DEFAULT_CAPACITY
+	end
+
+	def empty?
+		@bikes.empty?
+	end
+
+	public
 
 	def release_bike
-		fail 'No bikes available!' unless @bike
-		@bike
+		fail 'No bikes available!' if empty?
+		@bikes.pop
 	end
 
 	def dock(bike)
-		fail 'Station is full!' if @bike
-		@bike = bike
+		fail 'Station is full!' if full?
+		@bikes << bike
 	end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,28 @@ require 'bike'
 
 describe DockingStation do
   bike = Bike.new
+
+  it 'demonstrates default capacity when instantiating' do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+
+  # describe '#initialize' do
+  #   it 'varies capacity' do
+  #     station = DockingStation.new(33)
+  #     33.times { station.dock Bike.new }
+  #     expect { station.dock Bike.new }.to raise_error(RuntimeError, 'Station is full!')
+  #   end
+  # end
+
+  describe '#initialize' do
+    it 'defaults to class constant' do
+      DockingStation::DEFAULT_CAPACITY.times do
+        subject.dock(bike)
+      end
+      expect{ subject.dock(bike) }.to raise_error 'Station is full!'
+    end
+  end
+
   describe '#release_bike' do
     it { is_expected.to respond_to :release_bike }
 
@@ -26,12 +48,12 @@ describe DockingStation do
     it { is_expected.to respond_to(:dock).with(1).argument}
 
     it 'docks the given bike' do
-      expect(subject.dock(bike)).to eq bike
+      expect(subject.dock(bike)).to eq [bike]
     end
 
-    it 'raises an error if one bike already docked' do
-      subject.dock(bike)
-      expect { subject.dock(bike) }.to raise_error(RuntimeError, 'Station is full!')
+    it 'raises an error if full' do
+      subject.capacity.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error(RuntimeError, 'Station is full!')
     end
 
   end
